d82bb9abedbfe84cf3df0a2d4c655d025786175d
Renaldo <renaldo.surya@ti.ukdw.ac.id>
2023-11-13 22:42:31 +0700

    add: clinet dll [mumet]

---
+++ client/src/components/posts/styles.jsx
@@ -0,0 +1,14 @@
+const PostsStyle = ((theme) => ({
+   mainContainer: {
+      display: "flex",
+      alignItems: "center",
+   },
+   smMargin: {
+      margin: theme.spacing(1),
+   },
+   actionDiv: {
+      textAlign: "center",
+   },
+}));
+
+export default PostsStyle;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
